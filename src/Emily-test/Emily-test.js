77a6f39d1a0c67a15955e61db2a97b98d9b589af
pode.fr <pode.fr@gmail.com>
2011-10-12 17:35:24 +0200

    added dependency injection! services can be developped in isolation

--- src/Emily-test/Emily-test.js
+++ src/Emily-test/Emily-test.js
@@ -1,20 +1,66 @@
 TestCase("Emily", {
 	
+	setUp: function () {
+		this.spy = sinon.spy();
+	},
+	
 	"test should declare a service" : function () {
-		var spy = sinon.spy();
-		
-		assertTrue(Emily.declare("myService", spy));
-		assertSame(Emily, spy.args[0][0]);
+		assertTrue(Emily.declare("myService", this.spy));
+		assertSame(Emily, this.spy.args[0][0]);
 	},
 	
 	"test should require a service": function () {
-		var spy = sinon.spy();
-		
-		Emily.declare("myService", spy);
-		
+		Emily.declare("myService", this.spy);
 		assertObject(Emily.require("myService"));
 		assertUndefined(Emily.require("nothing"));
+	},
+	
+	"test should make Emily run in test mode": function () {
+		// Test mode should be false by default
+		assertFalse(Emily.getUnitTestMode());
+		Emily.setUnitTestMode(true);
+		assertTrue(Emily.getUnitTestMode()); 
+		Emily.setUnitTestMode(false);
+		assertFalse(Emily.getUnitTestMode());
+	},
+	
+	"test Emily should allow for dependency injection while in UnitTestMode": function () {
+		
+		// Define a future new service
+		var newService = function (API) {
+				this.requireExternal = function () {
+					// That will require an other one
+					return API.require("requiredService");
+				};
+			},
+			// This is the other service that will be required by the previously defined
+			requiredService = function (API) {},
+			// This is the same external service but stubbed
+			requiredServiceStubbed = {},
+			service;
+		
+		// Declare the future required service
+		Emily.declare("requiredService", requiredService);
+		// Declare the new service
+		Emily.declare("newService", newService);
+		// Require it to manipulate it
+		service = Emily.require("newService");
+		// The requiredService should be the same than the one previously declared
+		assertSame(requiredService, service.requireExternal().constructor);
+
+		// Now, in UnitTestMode		
+		Emily.setUnitTestMode(true);	
+		// Should first get undefined when requiring unstubbed service
+		assertUndefined(service.requireExternal());
+		// I can inject a stubbed service
+		Emily.inject("requiredService", requiredServiceStubbed);
+		// And it should now be the one that is required
+		assertSame(requiredServiceStubbed, service.requireExternal());
+	
+	},
+	
+	tearDown: function () {
+		// Important that UnitTestMode goes back to false
+		Emily.setUnitTestMode(false);
 	}
 });
-
-
--- src/Emily/Emily.js
+++ src/Emily/Emily.js
@@ -7,22 +7,58 @@ function Emily() {
 	 * The list of services
 	 * @private
 	 */
-	var _services = {};
+	var _services = {},
+		_injected = {},
+		_unitTestMode = false;
 	
     /**
-     * declare a new service
+     * Declare a new service
      * @param {String} name the name of the service
      * @param {Function} func the service's function
      */
-    this.declare = function declare(name, func) {
+    this.declare = function declare(name, func) {   	
     	_services[name] = new func(this);
     	return true;
     };
     
+    /**
+     * Require a service
+     * @param {String} name the name of the service
+     * @returns {Object} the service
+     */
     this.require = function require(name) {
-    	return _services[name];
+    	if (_unitTestMode) {
+    		return _injected[name];
+    	} else {
+    		return _services[name];	
+    	}
     };
     
+    /**
+     * Switch to unit testing. In unit testing, services run in isolation.
+     * To stubb a service, use the inject function
+     * @param value
+     */
+    this.setUnitTestMode = function setUnitTestMode(value) {
+    	_unitTestMode = value;
+    };
+    
+    /**
+     * Know the unit testing mode value
+     * @returns {Boolean}
+     */
+    this.getUnitTestMode = function getUnitTestMode() {
+    	return _unitTestMode;
+    };
+    
+    /**
+     * Inject a stubbed service
+     * @param {String} name the name of the service to stubb
+     * @param {Object} obj the stubbed service
+     */
+    this.inject = function inject(name, obj) {
+    	_injected[name] = obj;
+    };
     
     
 };
