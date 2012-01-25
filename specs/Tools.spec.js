f4420d8e845f0c5c90b4413835a16c90fe4c62eb
podefr <pode.fr@gmail.com>
2012-01-25 18:07:06 +0100

    modified Tools.loop so it can also work with nodelist if used in a browser

--- specs/Tools.spec.js
+++ specs/Tools.spec.js
@@ -183,6 +183,16 @@ require(["Tools"], function (Tools) {
 			Tools.loop(object, spy, thisObj);
 			expect(spy.mostRecentCall.object).toBe(thisObj);
 		});
+		
+		it("should iterate through nodelist", function () {
+			if (typeof document != "undefined") {
+				var dom = document.createElement("div"),
+					spy = jasmine.createSpy();
+				dom.innerHTML = "<p></p><p></p><p></p><p></p>";
+				Tools.loop(dom.querySelectorAll("p"), spy);
+				expect(spy.callCount).toEqual(4);
+			}
+		});
 	});
 	
 	describe("ToolsTestObjectDiff", function () {
--- src/Tools.js
+++ src/Tools.js
@@ -79,11 +79,15 @@ function Tools(){
 		 * @returns {Boolean} true if executed
 		 */
 		loop: function loop(iterated, callback, scope) {
-			var i;
+			var i, 
+				length;
 			
 			if (iterated instanceof Object && typeof callback == "function") {
-				if (iterated instanceof Array) {
-					iterated.forEach(callback, scope);
+				length = iterated.length;
+				if (length) {
+					for (i=0; i<length; i++) {
+						callback.call(scope, iterated[i], i, iterated);
+					}
 				} else {
 					for (i in iterated) {
 						if (iterated.hasOwnProperty(i)) {
