388187f7c47ad497a4861fb0ed0ab0627b8f6540
Olivier Scherrer <pode.fr@gmail.com>
2012-03-23 13:12:06 +0100

    updated archive

--- build/Emily.js
+++ build/Emily.js
@@ -4,20 +4,6 @@
  The MIT License (MIT)
 
  Copyright (c) 2012 Olivier Scherrer <pode.fr@gmail.com>
-
- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
- documentation files (the "Software"), to deal in the Software without restriction, including without limitation
- the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
- and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all copies or substantial 
- portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
- TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
- CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- IN THE SOFTWARE.
 */
 define("CouchDBStore",["Store","StateMachine","Tools","Promise"],function(a,d,h,e){function c(){var a=null,b={},g=new e,c={getView:function(){b.query=b.query||{};b.query.update_seq=true;a.request("CouchDB",{method:"GET",path:"/"+b.database+"/_design/"+b.design+"/_view/"+b.view,query:b.query},function(b){b=JSON.parse(b);this.reset(b.rows);i.event("subscribeToViewChanges",b.update_seq)},this)},getDocument:function(){a.request("CouchDB",{method:"GET",path:"/"+b.database+"/"+b.document,query:b.query},
 function(b){b=JSON.parse(b);b._id?(this.reset(b),i.event("subscribeToDocumentChanges")):g.reject(this)},this)},createDocument:function(){a.request("CouchDB",{method:"PUT",path:"/"+b.database+"/"+b.document,headers:{"Content-Type":"application/json"},data:this.toJSON()},function(){i.event("subscribeToDocumentChanges")})},subscribeToViewChanges:function(g){h.mixin({feed:"continuous",heartbeat:2E4,since:g},b.query);this.stopListening=a.listen("CouchDB",{path:"/"+b.database+"/_changes",query:b.query},
