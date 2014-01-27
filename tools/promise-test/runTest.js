8343c0d987580375e09d8d3df75305ef7d21b48f
Olivier Scherrer <pode.fr@gmail.com>
2014-01-26 21:12:48 -0500

    update promise A+ test runner

--- tools/promise-test/runTest.js
+++ tools/promise-test/runTest.js
@@ -1,38 +1,19 @@
-/* BROKEN as of latest promise A+ because of API change (!)
 var promiseTests = require("promises-aplus-tests"),
 	Promise = require("../../src/Promise");
 
 var adapter = {
-
-	resolved: function (value) {
-		var promise = new Promise;
-		promise.fulfill(value);
-		return promise;
-	},
-
-	rejected: function (reason) {
-		var promise = new Promise;
-		promise.reject(reason);
-		return promise;
-	},
-
-	pending: function () {
-		var promise = new Promise;
-	},
 
 	deferred: function () {
 		var promise = new Promise;
 
 		return {
 			promise: promise,
-		    fulfill: promise.fulfill.bind(promise),
-		    reject: promise.reject.bind(promise)
+		    resolve: promise.fulfill,
+		    reject: promise.reject
 		}
 	}
 };
 
-promiseTests(adapter, function () {
+promiseTests(adapter, { grep: "2" },function () {
 // All done, output in the CLI.
 });
-*/
-
